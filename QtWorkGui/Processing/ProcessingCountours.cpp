a4f0a9d472a617e6e10966ebadf195899a51d24f
Fullback22 <andrew-sergeenko@mail.ru>
2021-07-08 11:49:13 +0300

    change draw contours

--- QtWorkGui/Processing/ProcessingCountours.cpp
+++ QtWorkGui/Processing/ProcessingCountours.cpp
@@ -10,17 +10,12 @@ ProcessingCountours::ProcessingCountours(const ProcessingCountours& drop):
     resultImage_{drop.resultImage_ },
     threshold_{drop.threshold_}
 {
-
-    //masterContours_ = drop.masterContours_;
-    //masterHiararchy_ = drop.masterHiararchy_;
-
 }
 
 ProcessingCountours::ProcessingCountours(ProcessingCountours&& drop):
     resultImage_{ drop.resultImage_ },
     threshold_{ drop.threshold_ }
 {
-
 }
 
 void ProcessingCountours::performProcessing(cv::Mat const* inputImage)
@@ -31,23 +26,27 @@ void ProcessingCountours::performProcessing(cv::Mat const* inputImage)
     //cv::Mat coreMorfologis(cv::Size(5, 5), CV_8U);
     //cv::morphologyEx(cannyOut, cannyOut, cv::MORPH_ERODE, coreMorfologis);
     cv::Canny(*inputImage, cannyOut, threshold_, threshold_ * 2, 3, false);
-    //masterContours_.clear();
-    //masterContours_.resize(0);
-    //masterHiararchy_.clear();
-    //masterHiararchy_.resize(0);
     std::vector<std::vector<cv::Point>> contours;
     std::vector<cv::Vec4i> hiararchy;
     cv::findContours(cannyOut, contours, hiararchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    drawCountours(inputImage, contours);
-    /*for (size_t i{ 0 }; i < contours.size(); ++i)
+    resultImage_ = cv::Mat(inputImage->size(), CV_8UC1, cv::Scalar(0));
+    for (size_t i{ 0 }; i < contours.size(); ++i)
     {
-        cv::drawContours(resultImage_, resultImage_, int(i), cv::Scalar(0, 255, 0), 2);
-    }*/
+        cv::drawContours(resultImage_, contours, int(i), 255, 2);
+    }
 }
 
 void ProcessingCountours::drawProcessing(cv::Mat& inOutPutImage)
 {
-    inOutPutImage = resultImage_;
+    cv::cvtColor(inOutPutImage, inOutPutImage, CV_RGB2GRAY);
+    cv::cvtColor(inOutPutImage, inOutPutImage, CV_GRAY2RGB);
+    cv::Mat objectContour(inOutPutImage.size(), inOutPutImage.type(), cv::Scalar(0, 255, 0));
+    cv::Mat bufer(resultImage_);
+    cv::cvtColor(bufer, bufer, CV_GRAY2BGR);
+    cv::bitwise_and(bufer, objectContour, objectContour);
+    cv::bitwise_not(bufer, bufer);
+    cv::bitwise_and(inOutPutImage, bufer, inOutPutImage);
+    cv::bitwise_or(inOutPutImage, objectContour, inOutPutImage);
 }
 
 void ProcessingCountours::setThreshold(std::vector<int> newThreshold)
