fa05a28a8e39a9b805a45da52662dd8e95f7f02f
Fullback22 <andrew-sergeenko@mail.ru>
2021-09-05 20:33:39 +0300

    change computeComparis in ProcessingCountours

--- QtWorkGui/Processing/ProcessingCountours.cpp
+++ QtWorkGui/Processing/ProcessingCountours.cpp
@@ -80,7 +80,13 @@ void ProcessingCountours::getThreshold(std::vector<int>& outThreshold)
 
 int ProcessingCountours::computeComparsion(bool const isSingelThresold, std::vector<int>& const comparsionThreshold, cv::Mat* const masterImages, QtRotateRect roi)
 {
-    cv::Mat matchedPart(masterImages->size(),CV_8UC1);
+    cv::Mat matchedPart(masterImages->size(),CV_8UC1, cv::Scalar(0));
+    cv::Point vertices[4]{cv::Point(roi.getUpLeft_X() - roi.getMin_X(),roi.getUpLeft_Y() - roi.getMin_Y()),
+                          cv::Point(roi.getUpRigth_X() - roi.getMin_X(),roi.getUpRigth_Y() - roi.getMin_Y()) ,
+                          cv::Point(roi.getDownRigth_X() - roi.getMin_X(),roi.getDownRigth_Y() - roi.getMin_Y()) ,
+                          cv::Point(roi.getDownLeft_X() - roi.getMin_X(),roi.getDownLeft_Y() - roi.getMin_Y()) };
+    cv::fillConvexPoly(matchedPart, vertices, 4, cv::Scalar(255), 8);
+
     cv::bitwise_and(*masterImages, processingImage_, matchedPart);
     cv::Mat bufer_(processingImage_);
     cv::Mat mismatchedPart_1(masterImages->size(), CV_8UC1);
